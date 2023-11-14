566f82d5b146e7f345b16cb9d4fcf61af60b7a31
Tarikul69 <tarikulabir69@gmail.com>
2023-11-14 14:50:56 +0600

    Update

--- lib/screen/mycourses.dart
+++ lib/screen/mycourses.dart
@@ -4,6 +4,29 @@ class mycourses extends StatelessWidget {
   const mycourses({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: const Color.fromARGB(25, 27, 27, 27),
+      appBar: AppBar(
+        // foregroundColor: Colors.black,
+        backgroundColor: const Color.fromARGB(255, 27, 27, 27),
+        centerTitle: true,
+        title: const Text(" My Course"),
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: [
+            TextField(
+              decoration: InputDecoration(
+                iconColor: Colors.white,
+                suffixIcon: Icon(Icons.search),
+                hintText: "Abcd",
+                border: OutlineInputBorder(),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
