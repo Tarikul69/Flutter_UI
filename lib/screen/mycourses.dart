7415b35a96248c26317f12b9a54ea4c78a6d2860
Tarikul69 <tarikulabir69@gmail.com>
2023-11-14 14:14:41 +0600

    1st commit

---
+++ lib/screen/mycourses.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class mycourses extends StatelessWidget {
+  const mycourses({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
