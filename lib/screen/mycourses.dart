a8e48399882873d2b7bd2b50258228ee53baf302
Tarikul69 <tarikulabir69@gmail.com>
2023-11-15 14:30:28 +0600

    My Course

--- lib/screen/mycourses.dart
+++ lib/screen/mycourses.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:ui/screen/home.dart';
 
 class mycourses extends StatelessWidget {
   const mycourses({Key? key}) : super(key: key);
@@ -13,20 +14,91 @@ class mycourses extends StatelessWidget {
         title: const Text(" My Course"),
       ),
       body: Container(
-        padding: EdgeInsets.all(10),
+        padding: const EdgeInsets.all(10),
         child: Column(
           children: [
             TextField(
               decoration: InputDecoration(
                 iconColor: Colors.white,
                 suffixIcon: Icon(Icons.search),
-                hintText: "Abcd",
+                hintText: "Search coursr/mentor...",
                 border: OutlineInputBorder(),
               ),
+            ),
+            DefaultTabController(
+              length: 2,
+              child: Container(
+                height: 80,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.all(Radius.circular(40)),
+                ),
+                child: TabBar(
+                  physics: ScrollPhysics(),
+                  labelColor: Colors.black,
+                  dividerColor: Colors.amber,
+                  labelStyle: TextStyle(),
+                  tabs: [
+                    Tab(
+                      child: Text("Active Course"),
+                    ),
+                    Tab(
+                      child: Text("Download"),
+                    )
+                  ],
+                ),
+              ),
+              // child: Column(
+              //   children: [
+              //     Material(
+              //       child: Container(
+              //         child: TabBar(
+              //           physics: ClampingScrollPhysics(),
+              //           tabs: [
+              //             Tab(
+              //               child: Container(
+              //                 height: 35,
+              //                 decoration: BoxDecoration(
+              //                   borderRadius: BorderRadius.circular(20),
+              //                   border: Border.all(color: Colors.black26),
+              //                 ),
+              //                 child: Align(
+              //                   alignment: Alignment.center,
+              //                   child: Text("Chat"),
+              //                 ),
+              //               ),
+              //             ),
+              //             Tab(
+              //               child: Container(
+              //                 height: 35,
+              //                 decoration: BoxDecoration(
+              //                   borderRadius: BorderRadius.circular(20),
+              //                   border: Border.all(color: Colors.black26),
+              //                 ),
+              //                 child: Align(
+              //                   alignment: Alignment.center,
+              //                   child: Text("Chat"),
+              //                 ),
+              //               ),
+              //             ),
+              //           ],
+              //         ),
+              //       ),
+              //     ),
+              //     Expanded(
+              //       child: TabBarView(
+              //         children: [
+              //           home(),
+              //           home(),
+              //         ],
+              //       ),
+              //     ),
+              //   ],
+              // ),
             ),
           ],
         ),
       ),
+      
     );
   }
 }
