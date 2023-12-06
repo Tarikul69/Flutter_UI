1ef97e546ea8bbff4fc46ad2f9707bac8ae43c77
Tarikul69 <tarikulabir69@gmail.com>
2023-12-06 18:08:57 +0600

    Review

--- lib/screen/review/about.dart
+++ lib/screen/review/about.dart
@@ -7,145 +7,245 @@ class about extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(8.0),
-        child: Container(
-          alignment: Alignment.topLeft,
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "Overview",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
-                ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.video_file,
-                  color: Color.fromARGB(255, 118, 117, 117),
-                ),
-                label: Text(
-                  "1h 30m on-demand video",
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Container(
+            alignment: Alignment.topLeft,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const Text(
+                  "Overview",
                   style: TextStyle(
-                    color: Color.fromARGB(255, 118, 117, 117),
                     fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                    fontSize: 18,
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.pageview_outlined,
-                  color: Color.fromARGB(255, 118, 117, 117),
+                const SizedBox(
+                  height: 15,
                 ),
-                label: Text(
-                  "5 curriculum",
-                  style: TextStyle(
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.video_file,
                     color: Color.fromARGB(255, 118, 117, 117),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                  ),
+                  label: Text(
+                    "1h 30m on-demand video",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.download,
-                  color: Color.fromARGB(255, 118, 117, 117),
+                SizedBox(
+                  height: 15,
                 ),
-                label: Text(
-                  "15 downloadable resources",
-                  style: TextStyle(
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.pageview_outlined,
                     color: Color.fromARGB(255, 118, 117, 117),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                  ),
+                  label: Text(
+                    "5 curriculum",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.timeline_rounded,
-                  color: Color.fromARGB(255, 118, 117, 117),
+                SizedBox(
+                  height: 15,
                 ),
-                label: Text(
-                  "Full lifetime access",
-                  style: TextStyle(
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.download,
                     color: Color.fromARGB(255, 118, 117, 117),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                  ),
+                  label: Text(
+                    "15 downloadable resources",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.man,
-                  color: Color.fromARGB(255, 118, 117, 117),
+                SizedBox(
+                  height: 15,
                 ),
-                label: Text(
-                  "Certificate of Completion",
-                  style: TextStyle(
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.timeline_rounded,
                     color: Color.fromARGB(255, 118, 117, 117),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                  ),
+                  label: Text(
+                    "Full lifetime access",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              TextButton.icon(
-                onPressed: () {},
-                icon: Icon(
-                  Icons.people,
-                  color: Color.fromARGB(255, 118, 117, 117),
+                SizedBox(
+                  height: 15,
                 ),
-                label: Text(
-                  "500+ Student",
-                  style: TextStyle(
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.man,
                     color: Color.fromARGB(255, 118, 117, 117),
-                    fontWeight: FontWeight.bold,
-                    fontSize: 17,
+                  ),
+                  label: Text(
+                    "Certificate of Completion",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Text(
-                "Mentor",
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18,
+                SizedBox(
+                  height: 15,
                 ),
-              ),
-              SizedBox(
-                height: 10,
-              ),
-              Row(
-                children: [],
-              )
-            ],
+                TextButton.icon(
+                  onPressed: () {},
+                  icon: Icon(
+                    Icons.people,
+                    color: Color.fromARGB(255, 118, 117, 117),
+                  ),
+                  label: Text(
+                    "500+ Student",
+                    style: TextStyle(
+                      color: Color.fromARGB(255, 118, 117, 117),
+                      fontWeight: FontWeight.bold,
+                      fontSize: 17,
+                    ),
+                  ),
+                ),
+                SizedBox(
+                  height: 15,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Mentor",
+                  style: TextStyle(
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+
+                // Card(
+                //   child: Row(
+                //     children: [
+                //       CircleAvatar(),
+                //       ListTile(
+                //         title: Text("data"),
+                //         subtitle: Text("data"),
+                //       ),
+                //     ],
+                //   ),
+                // )
+                // ListView(
+                //   children: [
+                //     ListTile(
+                //       title: Text("data"),
+                //       subtitle: Text("data"),
+                //     ),
+                //   ],
+                // )
+              ],
+            ),
           ),
         ),
       ),
--- lib/screen/review/curriculum.dart
+++ lib/screen/review/curriculum.dart
@@ -6,7 +6,233 @@ class curriculum extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        child: Text("Curriculum"),
+          child: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Column(
+          children: [
+            ListTile(
+              title: Text("Curriculum: What you will learn in this course"),
+            ),
+            ExpansionTile(
+              textColor: Colors.black,
+              title: Row(
+                children: [
+                  Text("Week1"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Title(
+                    color: Colors.black,
+                    child: Title(
+                      color: Colors.black,
+                      child: Text(
+                        "Introduction to UI/UX design",
+                        style: TextStyle(
+                          fontWeight: FontWeight.bold,
+                          fontSize: 20,
+                        ),
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              children: [
+                ListTile(
+                    subtitle: Title(
+                        color: Colors.black,
+                        child: Text(
+                            "Introduction to UI/UX design and animation providers and overview of "))),
+                ListTile(
+                  title: Title(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.watch),
+                            Text("5 hours to complete"),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.video_file),
+                            Text("4 Videos"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              textColor: Colors.black,
+              title: Row(
+                children: [
+                  Text("Week2"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      "Principal of animation in UI/UX",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              children: [
+                ListTile(
+                  subtitle: Text(
+                      "Introduction to UI/UX design and animation providers and overview of "),
+                ),
+                ListTile(
+                  title: Title(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.watch),
+                            Text("5 hours to complete"),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.video_file),
+                            Text("4 Videos"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              textColor: Colors.black,
+              title: Row(
+                children: [
+                  Text("Week3"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      'Design tools for animation',
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              children: [
+                ListTile(
+                  subtitle: Text(
+                      "Introduction to UI/UX design and animation providers and overview of "),
+                ),
+                ListTile(
+                  title: Title(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.watch),
+                            Text("5 hours to complete"),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.video_file),
+                            Text("4 Videos"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            ExpansionTile(
+              textColor: Colors.black,
+              title: Row(
+                children: [
+                  Text("Week4"),
+                  SizedBox(
+                    width: 10,
+                  ),
+                  Title(
+                    color: Colors.black,
+                    child: Text(
+                      "Introduction to UI/UX design",
+                      style: TextStyle(
+                        fontWeight: FontWeight.bold,
+                        fontSize: 20,
+                      ),
+                    ),
+                  )
+                ],
+              ),
+              children: [
+                ListTile(
+                  subtitle: Text(
+                      "Introduction to UI/UX design and animation providers and overview of "),
+                ),
+                ListTile(
+                  title: Title(
+                    color: Colors.black,
+                    child: Column(
+                      children: [
+                        Row(
+                          children: [
+                            Icon(Icons.watch),
+                            Text("5 hours to complete"),
+                          ],
+                        ),
+                        SizedBox(
+                          height: 8,
+                        ),
+                        Row(
+                          children: [
+                            Icon(Icons.video_file),
+                            Text("4 Videos"),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                )
+              ],
+            ),
+            // Row(
+            //   children: [
+            //     Text('354'),
+            //     ElevatedButton(
+            //       onPressed: () {},
+            //       child: Text("Enroll Now"),
+            //     )
+            //   ],
+            // )
+          ],
+        ),
+      )
       ),
     );
   }
