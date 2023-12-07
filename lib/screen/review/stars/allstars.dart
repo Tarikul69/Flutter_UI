cba6cc93f90ca906eb5ec09cff610757cfffe8c0
Tarikul69 <tarikulabir69@gmail.com>
2023-12-07 17:44:15 +0600

    Review-Update

--- lib/screen/review/stars/allstars.dart
+++ lib/screen/review/stars/allstars.dart
@@ -5,8 +5,158 @@ class allstars extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(
-        child: Text("All Stars"),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.vertical,
+        child: Padding(
+          padding: EdgeInsets.all(10.0),
+          child: Column(
+            children: [
+              Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Terry Press",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                  ],
+                ),
+              ),
+              ListTile(
+                subtitle: Text("20 Feb 2023"),
+                title: Text("This course was comprehensive"),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Terry Press",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                  ],
+                ),
+              ),
+              ListTile(
+                subtitle: Text("20 Feb 2023"),
+                title: Text("This course was comprehensive"),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Terry Press",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                  ],
+                ),
+              ),
+              ListTile(
+                subtitle: Text("20 Feb 2023"),
+                title: Text("This course was comprehensive"),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Terry Press",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                  ],
+                ),
+              ),
+              ListTile(
+                subtitle: Text("20 Feb 2023"),
+                title: Text("This course was comprehensive"),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+              Container(
+                height: 30,
+                width: MediaQuery.of(context).size.width,
+                child: Row(
+                  children: [
+                    Text(
+                      "Terry Press",
+                      style: TextStyle(
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    SizedBox(width: 100),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                    Icon(Icons.star, color: Colors.amber),
+                  ],
+                ),
+              ),
+              ListTile(
+                subtitle: Text("20 Feb 2023"),
+                title: Text("This course was comprehensive"),
+              ),
+              Divider(
+                color: Colors.black,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
